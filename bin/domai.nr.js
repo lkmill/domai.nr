1b12ef17daa74b91a9af698178afe4a7cfba30f2
Linus Miller <lohfu@lohfu.io>
2016-04-18 12:42:55 +0200

    Set up using commandr, tape, custom TAP transform and ~/.domainrrc configuration.

--- bin/domai.nr.js
+++ bin/domai.nr.js
@@ -2,35 +2,75 @@
 
 'use strict';
 
+// modules > native
+const fs = require('fs');
+
+//modules > 3rd party
+const transform = require('../transform')();
 const request = require('superagent');
+const program = require('commander');
+const test = require('tape');
 
-const args = process.argv.slice(2);
-
-if (args.length < 2) {
-	console.log('USAGE: domai.nr MASHAPE_KEY domain [tLDs]');
-
-	// TODO decide what error code
-	process.exit(2);
+function list(input) {
+  return input.split(',');
 }
 
-let domains;
+const pkg = require('../package.json');
 
-if (args.length === 2)
-	domains = args[1];
-else
-	domains = args[2].split(',').map((tl) => args[1] + '.' + tl).join(',');
+/* TODO fix formatting totals (stream seems to end before tape outputs the
+ * results, however tape outputs it correctly if you pipe straight to stdout
+ * `test.createStream().pipe(process.stdout);` instead of
+ * `test.createStream().pipe(transform).pipe(process.stdout);`
+ * or
+ * `test.createStream().pipe(require('tap-spec')()).pipe(process.stdout);`
+ */
+test.createStream().pipe(transform).pipe(process.stdout);
 
-request.get('https://domainr.p.mashape.com/v2/status?domain=' + domains)
-	.set('X-Mashape-Key', args[0])
-	.set('Accept', 'application/json')
-	.end((err, res) => {
-		if (err) {
-			console.error(err);
-			// TODO decide what error code
-			return process.exit(2);
-		}
+program
+  .version(pkg.version)
+  .usage('[options] <domains...>')
+  .option('-c, --config <path>', 'Location of domainr configuration file', '~/.domainrrc')
+  .option('-k, --key <value>', 'Mashape API Key for Domai.nr')
+  .option('-t, --tlds <items>', 'Comma seperated list of top level domains', list)
+  .option('-a, --all', 'Use ALL tLDs (overrides --tlds)')
+  .parse(process.argv);
 
-		console.log(res.body);
+const config = JSON.parse(fs.readFileSync('/home/sup3rman/.domainrrc', 'utf8'));
 
-		return process.exit(0);
-	});
+program.key = program.key || config.key;
+program.tlds = program.tlds || config.tlds;
+
+if (program.args.length === 0) {
+  program.help();
+  program.exit();
+}
+
+let domains = [];
+
+program.args.forEach((domain) => {
+  if (domain.indexOf('.') > -1)
+    domains.push(domain);
+  else
+    domains = domains.concat(program.tlds.map((tld) => domain + '.' + tld));
+});
+
+domains.sort();
+
+test('Domains', function (t) {
+  request.get('https://domainr.p.mashape.com/v2/status?domain=' + domains.join(','))
+    .set('X-Mashape-Key', program.key)
+    .set('Accept', 'application/json')
+    .end((err, res) => {
+      if (err) {
+        console.error(err);
+        // TODO decide what error code
+        return process.exit(2);
+      }
+
+      res.body.status.forEach(function (status) {
+        t.equal(status.summary, 'inactive', status.domain);
+      });
+
+      t.end();
+    });
+});
---
+++ transform.js
@@ -0,0 +1,83 @@
+'use strict';
+
+const tapOut = require('tap-out');
+const through = require('through2');
+const duplexer = require('duplexer');
+const format = require('chalk');
+const prettyMs = require('pretty-ms');
+const _ = require('lodash');
+const symbols = require('figures');
+
+module.exports = function (spec) {
+  spec = spec || {};
+
+  const OUTPUT_PADDING = spec.padding || '  ';
+
+  const output = through();
+  const parser = tapOut();
+  const stream = duplexer(parser, output);
+  const startTime = new Date().getTime();
+
+  output.push('\n');
+
+  parser.on('test', function (test) {
+    output.push('\n' + pad(format.underline(test.name)) + '\n\n');
+  });
+
+  // Passing assertions
+  parser.on('pass', function (assertion) {
+    const glyph = format.green(symbols.tick);
+    const name = format.dim(assertion.name);
+
+    output.push(pad('  ' + glyph + ' ' + name + '\n'));
+  });
+
+  // Failing assertions
+  parser.on('fail', function (assertion) {
+    const glyph = symbols.cross;
+    const title = glyph + ' ' + assertion.name;
+
+    output.push(pad('  ' + format.red(title) + '\n'));
+    stream.failed = true;
+  });
+
+  parser.on('comment', function (comment) {
+    output.push(pad('  ' + format.yellow(comment.raw)) + '\n');
+  });
+
+  // All done
+  parser.on('output', function (results) {
+    console.log('all done');
+    output.push('\n\n');
+
+    output.push(formatTotals(results));
+    output.push('\n\n\n');
+
+    // Exit if no tests run. This is a result of 1 of 2 things:
+    //  1. No tests were written
+    //  2. There was some error before the TAP got to the parser
+    if (results.tests.length === 0) {
+      process.exit(1);
+    }
+  });
+
+  // Utils
+  function formatTotals(results) {
+    if (results.tests.length === 0) {
+      return pad(format.red(symbols.cross + ' No tests found'));
+    }
+
+    return _.filter([
+      pad('total:     ' + results.asserts.length),
+      pad(format.green('passing:   ' + results.pass.length)),
+      results.fail.length > 0 ? pad(format.red('failing:   ' + results.fail.length)) : undefined,
+      pad('duration:  ' + prettyMs(new Date().getTime() - startTime))
+    ], _.identity).join('\n');
+  }
+
+  function pad(str) {
+    return OUTPUT_PADDING + str;
+  }
+
+  return stream;
+};
